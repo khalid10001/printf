989ade05142515ce9a2a78bd3833b6fc7a61208a
bouabsaamina <amina.bouabsa@univ-constantine2.dz>
2023-09-17 05:22:22 -0700

    handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -19,7 +19,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'c', print_char}, {'s', print_str}, {'%', print_per},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsign}, {'o', print_oct}, {'x', print_hexadec},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'X', print_hexa_up}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
